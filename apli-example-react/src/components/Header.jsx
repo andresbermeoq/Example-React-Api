ec55d2b85abeb47f9315ae46a4be8fa361196671
andipandiber <andipandi467@gmail.com>
2021-04-16 01:07:47 -0500

    Add Github Docs

--- apli-example-react/src/components/Header.jsx
+++ apli-example-react/src/components/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => (
      <header className="header">
           <nav className="navbar navbar-expand-lg navbar-dark bg-light justify-content-center">
                <ul className="nav nav-pills justify-content-center">
-                    <li className="nav-item btn btn-outline-success m-1">
+                    <li className="nav-item btn btn-outline-info btn-outline-success m-1">
                          <Link className="nav-link" to="/">Inicio</Link>
                     </li>
-                    <li className="nav-item btn btn-outline-success m-1">
+                    <li className="nav-item btn btn-outline-info btn-outline-success m-1">
                          <Link className="nav-link" to="/country">Paises</Link>
                     </li>
                </ul>
